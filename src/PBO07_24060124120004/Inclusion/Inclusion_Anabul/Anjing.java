147fe79905d01ce04d8c93088dcd534039120fc2
{Kayla Febrina Laura Ayu} <{email github}>
2026-04-27 23:10:33 +0700

    Pertemuan 7 selesai

---
+++ src/PBO07_24060124120004/Inclusion/Inclusion_Anabul/Anjing.java
@@ -0,0 +1,22 @@
+package Inclusion.Inclusion_Anabul;
+/* Nama File    : Anjing.java
+ * NIM          : 24060124120004  
+ * Nama         : Kayla Febrina Laura Ayu 
+ * Tanggal      : 22 April 2026
+ */
+
+public class Anjing extends Anabul {
+    public Anjing(String Nama) {
+        super(Nama);
+    }
+
+    @Override
+    public void Gerak() {
+        System.out.println(Nama + " (Anjing) Gerak: Melata");
+    }
+
+    @Override
+    public void Bersuara() {
+        System.out.println(Nama + " (Anjing) Bersuara: Guk-guk");
+    }
+}
---
+++ src/PBO07_24060124120004/Inclusion/Inclusion_Civitasakademika/Mahasiswa.java
@@ -0,0 +1,36 @@
+package Inclusion.Inclusion_Civitasakademika;
+/* Nama File    : Mahasiswa.java
+ * NIM          : 24060124120004  
+ * Nama         : Kayla Febrina Laura Ayu 
+ * Tanggal      : 22 April 2026
+ */
+
+/* a.Kelas Mahasiswa */
+public class Mahasiswa extends Civitasakademika {
+    private String NIM;
+    private Dosen Dosenwali;
+
+    public Mahasiswa(String Nama, String NIM, Dosen Dosenwali) {
+        super(Nama);
+        this.NIM = NIM;
+        this.Dosenwali = Dosenwali;
+    }
+
+    @Override
+    public String getNomor() {
+        return NIM;
+    }
+
+    /* i.Prosedur setWali untuk mengubah isian komponen dosenwali dengan parameter input dosen */
+    public void setWali(Dosen d) {
+        this.Dosenwali = d;
+    }
+
+    /* j. Prosedur tampilDataMahasiswa untuk menampilkan data NIM,
+          Nama Mahasiswa, dan Nama Dosenwali */
+    public void tampilDataMahasiswa() {
+        System.out.println("NIM: " + NIM);
+        System.out.println("Nama: " + Nama);
+        System.out.println("Dosen Wali: " + Dosenwali.getNama());
+    }
+}
